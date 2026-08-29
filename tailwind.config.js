7393b52ab88848d3299aaf755a471fdb0dc63475
2550rayhanpc <2550rayhanpc@gmail.com>
2026-08-29 06:39:56 -0600

    Configure Tailwind CSS with custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,14 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-hind-siliguri)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
